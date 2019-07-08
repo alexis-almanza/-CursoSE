6cb50a5c01caf3a28310e4c55d10b12e2f51cd93
alexis-almanza <alexis.edao@gmail.com>
2019-07-07 20:51:49 -0500

    switch int

---
+++ src/cursose/Switch.java
@@ -0,0 +1,23 @@
+package cursose;
+
+public class Switch {
+	public static void main(String[] args) {
+		int valor = 5;
+		
+		switch(valor) {
+		case 1: 
+			System.out.println("valor 1 "); 
+			break;
+		case 2:
+			System.out.println("valor 2 "); 
+			break;
+		case 3:
+			System.out.println("valor 3 "); 
+			break;
+			default:
+				System.out.println("no se encontro valor "); 
+				break;
+		}
+	}
+
+}
