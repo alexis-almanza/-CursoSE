f283328309943352173c4ff49e99f215bd15a366
alexis-almanza <alexis.edao@gmail.com>
2019-07-07 20:43:05 -0500

    vector

---
+++ src/cursose/Vector.java
@@ -0,0 +1,16 @@
+package cursose;
+
+public class Vector {
+	public static void main(String[] args) {
+		String[] vector = new String[3];
+		vector[0] = "Alexis";
+		vector[1] = "Gera";
+		vector[2] = "Jesus";
+		//vector[3] = "algo";
+		
+		for(String valor: vector) {
+			System.out.println(valor);
+		}
+		
+	}
+}
