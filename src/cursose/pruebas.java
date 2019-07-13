43ff4835f97fbc1cdfc8fc445bdc0c06d496ef97
alexis-almanza <alexis.edao@gmail.com>
2019-07-13 10:07:47 -0500

    complemento herencia

--- src/cursose/pruebas.java
+++ src/cursose/pruebas.java
@@ -3,21 +3,15 @@ package cursose;
 public class pruebas {
 	public static void main(String[] args) {
 		
-		_25_getSet sc = new _25_getSet();
-		sc.setNombre("Alexis");
-		sc.setApellido("Almanza Ortega");
-		sc.setColor("GREEN");
+		perros sc = new perros("hachi", "hachikoo", 12, "pastor belga");
+
+		/*perros sc = new perros();
+		sc.setApodo("hachikooo");
+		sc.setEdad(17);
+		sc.setNombre("hachi");
+		sc.setRaza("pastor belga");*/
 		
-		String apellido = sc.getApellido();
-		String nombre = sc.getNombre();
-		System.out.println(nombre);
-		System.out.println(apellido);
-		System.out.println(sc.getColor());
-		
-		
-		//el set y get se utiliza para poder retornar los valores de ambito private
-		
-		
+		sc.mostrar();
 	}
 
 }
