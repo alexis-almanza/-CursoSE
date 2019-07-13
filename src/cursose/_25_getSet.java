8948479c95fbc29649bf9dd4f358f892073d2167
alexis-almanza <alexis.edao@gmail.com>
2019-07-12 22:48:28 -0500

    setear

---
+++ src/cursose/_25_getSet.java
@@ -0,0 +1,34 @@
+package cursose;
+
+public class _25_getSet {
+	
+	private String nombre;
+	private String apellido;
+	private String color;
+	
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public String getApellido() {
+		return this.apellido;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public String getColor() {
+		return this.color;
+		
+		
+	}
+}
